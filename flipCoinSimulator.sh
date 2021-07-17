a32f446aa1806ad1d25e32ebe77cf37327296fad
Vinod Kumar R <vk4765712@gmail.com>
2021-07-17 22:53:47 +0530

    added: multiple flip generator

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -4,16 +4,32 @@ echo "Welcome to Coin Flip Simulator Program"
 
 
 # Variable
+headCount=0
+tailCount=0
+
+MAX_PLAY=10
 HEAD=1
 
 
-# Generate Head/Tail
-coinFlip=$(( RANDOM % 2 ))
+# Simulate multiple coin flips
+while [ $headCount -lt $MAX_PLAY -a $tailCount -lt $MAX_PLAY ]
+do
+
+	# Generate Head/Tail
+        coinFlip=$(( $RANDOM %2 ))
 
 
-# Check if it's Head or Tail
-if [ $coinFlip -eq $HEAD ]; then
-	echo "heads"
-else
-	echo "tails"
-fi
+	# Check if it's Head or Tail
+        if [ $coinFlip -eq $HEAD ]; then
+                (( headCount++ ))
+        else
+                (( tailCount++ ))
+        fi
+
+done
+
+
+# Print the final score
+echo "Head $headCount"
+echo "Tail $tailCount"
+
